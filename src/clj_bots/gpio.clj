8e13a175650a62fcddf314492091c480254770bf
Darrell Hamilton <darrell.noice@gmail.com>
2014-10-30 15:37:56 -0600

    basic sysfs implementation

--- src/clj_bots/gpio.clj
+++ src/clj_bots/gpio.clj
@@ -1,6 +1,52 @@
 (ns clj-bots.gpio)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def edge-detection-modes #{:none :rising :falling :both})
+(def states #{:high :low})
+(def directions #{:input :output})
+
+(defprotocol GPIOPin
+  (pin [this])
+  (direction [this])
+  (set-direction! [this d] [this d state])
+  (state [this])
+  (set-state! [this s]))
+
+(defn input?
+  "Test if the pin is current configured as an input pin."
+  [p]
+  (= (direction p) :input))
+
+(defn input!
+  "Configure the pin to be an input."
+  [p]
+  (set-direction! p :input))
+
+(defn output?
+  "Test if the pin is current configured as an output pin."
+  [p]
+  (= (direction p) :output))
+
+(defn output!
+  "Configure the pin to be an output."
+  [p]
+  (set-direction! p :output))
+
+(defn high?
+  "Test if the pin is currently :high."
+  [p]
+  (= (state p) :high))
+
+(defn low?
+  "Test if the pin is currently :low."
+  [p]
+  (= (state p) :low))
+
+(defn low!
+  "Set an output pin to :low."
+  [p]
+  (set-state! p :low))
+
+(defn high!
+  "Set an output pin to :high."
+  [p]
+  (set-state! p :high))
---
+++ src/clj_bots/impl/sysfs.clj
@@ -0,0 +1,79 @@
+(ns clj-bots.gpio.impl.sysfs
+  (:require [clj-bots.gpio :as gpio]
+            [clojure.java.io :refer (file)]))
+
+(def sysfs-root (file "/sys/class/gpio"))
+
+(defn- keyword-to-state [k]
+  (condp = k
+    :high "1"
+    :low "0"))
+
+(defn state-to-keyword [s]
+  (condp = s
+    "1" :high
+    "0" :low))
+
+(defn- keyword-to-direction [k]
+  (condp = k
+    :input "in"
+    :output "out"))
+
+(defn- keyword-to-initial-state [k]
+  (name k))
+
+(defn- direction-to-keyword [d]
+  (condp = d
+    "in" :input
+    "out" :output))
+
+(defn- read-file [f]
+  (trim-newline (slurp f)))
+
+(defrecord SysfsGPIOPin
+  [gpio-pin
+   d-file
+   v-file
+   e-file
+   al-file]
+  GPIOPin
+  (pin [this] gpio-pin)
+
+  (direction [this]
+    (let [content (read-file d-file)]
+      (direction-to-keyword content)))
+
+  (set-direction! [this d]
+    (let [v (keyword-to-direction d)]
+      (spit d-file v)))
+
+  (set-direction! [this d state]
+    (if (= d :output)
+      (spit d-file (keyword-to-initial-state state))
+      ; Blow up? input with an initial state doesn't make sense...
+      ))
+
+  (state [this]
+    (let [content (read-file v-file)]
+      (keyword-to-state content)))
+
+  (set-state! [this s]
+    (spit v-file (keyword-to-state s)))
+  )
+
+(defn reserve
+  ([pin-id] (reserve sysfs-root))
+  ([root pin-id]
+   (spit (file root "export") pin-id)
+   (let [pin-root (file root (str "gpio" pin-id))]
+     (->SysfsGPIOPin
+       (file pin-root "direction")
+       (file pin-root "value")
+       (file pin-root "edge")
+       (file pin-root "active_low"))))
+
+  (defn release [p]
+    (let [pin-id (cond
+                   (integer? p) p
+                   (instance? SysfsGPIOPin p) (pin p))]
+      (spit (file root "unexport") pin-id)))
